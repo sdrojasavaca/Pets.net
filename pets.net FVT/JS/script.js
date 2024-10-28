0810a48d9fc1928dcc24b7c0b99ae84c80f23f10
sdrojasavaca <162600858+sdrojasavaca@users.noreply.github.com>
2024-10-27 21:11:25 -0300

    Add files via upload

---
+++ pets.net FVT/JS/script.js
@@ -0,0 +1,4 @@
+document.getElementById('menuToggle').addEventListener('click', function () {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('collapsed');
+});
